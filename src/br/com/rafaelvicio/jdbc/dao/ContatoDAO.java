a6204831bd2ef8d9f2d2c48666dc6f8418589a2f
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-06-06 15:07:43 -0300

    Listando

--- src/br/com/rafaelvicio/jdbc/dao/ContatoDAO.java
+++ src/br/com/rafaelvicio/jdbc/dao/ContatoDAO.java
@@ -6,9 +6,12 @@ import br.com.rafaelvicio.jdbc.modelo.Contato;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class ContatoDAO {
 
@@ -38,5 +41,34 @@ public class ContatoDAO {
         } catch (SQLException e){
             throw new RuntimeException(e);
         }
+    }
+
+    public ResultSet getLista(){
+        PreparedStatement stmt = this.conection.prepareStatement("select * from contatos");
+        ResultSet rs = stmt.executeQuery();
+
+        List<Contato> contatos = new ArrayList<>();
+
+        while (rs.next()){
+
+            // Criando o objeto Contato
+            Contato contato = new Contato();
+            contato.setNome(rs.getString("nome"));
+            contato.setEmail(rs.getString("email"));
+            contato.setEndereco(rs.getString("endereco"));
+
+            //montando a data através do Calendar
+            Calendar data = Calendar.getInstance();
+            data.setTime(rs.getDate("dataNascimento"));
+            contato.setDataNascimento(data);
+
+            //adiciona o objeto á lista
+            contatos.add(contato);
+        }
+
+        rs.close();
+        stmt.close();
+
+        return contatos;
     }
 }
---
+++ src/br/com/rafaelvicio/jdbc/teste/TesteLista.java
@@ -0,0 +1,8 @@
+package br.com.rafaelvicio.jdbc.teste;
+
+import java.sql.PreparedStatement;
+
+public class TesteLista {
+
+    PreparedStatement stmt = this.co
+}
